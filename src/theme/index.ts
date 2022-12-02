4518743eeb0c978b1e4de42cb15c38f7d2ec7f05
Hasan Naseem <hasannaseem2@gmail.com>
2022-12-02 18:08:20 +0500

    Dummy Colors populated
    As discussed, I have only filled the light color palette object

--- src/theme/index.ts
+++ src/theme/index.ts
@@ -3,7 +3,28 @@ import { PaletteMode } from '@mui/material'
 const lightPalette = {
 	primary: {
 		main: '#267755',
-		contrastText: '#000'
+		contrastText: '#000',
+		light: '#26875e'
+	},
+	secondary: {
+		main: '#6d8726',
+		light: '#9fc92a'
+	},
+	error: {
+		main: '#d32f2f',
+		light: '#ef5350'
+	},
+	warning: {
+		main: '#ed6c02',
+		light: '#ff9800'
+	},
+	info: {
+		main: '#0288d1',
+		light: '#03a9f4'
+	},
+	success: {
+		main: '#2e7d32',
+		light: '#4caf50'
 	},
 	divider: '#555',
 	background: {
